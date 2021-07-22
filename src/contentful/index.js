101cd1b77061e13954f66a096ac7267730199431
Hannah <hannah.gunn@vodafone.com>
2021-07-22 15:41:08 +0100

    fix: 🐛 Preview and production environments working correctly

--- src/contentful/index.js
+++ src/contentful/index.js
@@ -2,6 +2,8 @@ import * as contentful from 'contentful';
 
 export default contentful.createClient({
   space: process.env.REACT_APP_API_SPACE,
-  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
-  host: 'preview.contentful.com'
+  accessToken: process.env.NODE_ENV === 'production' ?
+    process.env.REACT_APP_PUBLIC_TOKEN
+    : process.env.REACT_APP_PREVIEW_TOKEN,
+  host: process.env.NODE_ENV === 'production' ? "cdn.contentful.com" : "preview.contentful.com"
 });
